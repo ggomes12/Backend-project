3edbbf63fafd76a9a9615495e34a804f8e018444
Guilherme Gomes <guilherme.silva@ufpi.edu.br>
2024-03-24 21:02:31 -0300

    Mapping and Connecting Postgres

--- src/main/java/br/com/ggomes12/sistemaspringboot/entity/UserEntity.java
+++ src/main/java/br/com/ggomes12/sistemaspringboot/entity/UserEntity.java
@@ -1,4 +1,60 @@
 package br.com.ggomes12.sistemaspringboot.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false, unique = true, length = 100)
+    private String login;
+    @Column(nullable = false, length = 30)
+    private String password;
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
